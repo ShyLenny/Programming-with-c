e9ea50ddf5f3e642762669834c98987342096f18
Shalini <you@example.com>
2026-07-31 02:11:36 +0530

    program8

---
+++ class-01/08_age.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int bDay, bMonth, bYear;
+    int cDay, cMonth, cYear;
+    int age;
+
+    printf("Enter Date of Birth (DD MM YYYY): ");
+    scanf("%d %d %d", &bDay, &bMonth, &bYear);
+
+    printf("Enter Current Date (DD MM YYYY): ");
+    scanf("%d %d %d", &cDay, &cMonth, &cYear);
+
+    age = cYear - bYear;
+
+    printf("Age = %d years", age);
+
+    return 0;
+}
