cfc26bf23348c1a91033b6ce85599ab92e627552
Shalini <you@example.com>
2026-08-03 20:56:00 +0530

    prog4

---
+++ week_03/04_table_of_8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+
+    printf("Table of 8\n");
+
+    for (i = 1; i <= 10; i++)
+    {
+        printf("8 x %d = %d\n", i, 8 * i);
+    }
+
+    return 0;
+}
